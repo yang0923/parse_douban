fda67493317abbe8c8a138fd2ea405fb6b669b58
yy <yy1875737985>
2024-10-11 15:36:47 +0800

    feat(config): 更新日志配置，增加日志保存控制

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # 日志配置
-LOGGING_ENABLED = True  # 控制日志是否启用
+SAVE_LOGS = False  # 控制是否保存日志 到文件 Fasle:不保存 True:保存
+LOGGING_ENABLED = True  # 控制日志是否启用 , Fasle:不启用 True:启用
 LOG_FILE_PATH = BASE_DIR / 'logs' / 'app.log'  # 设置日志文件路径
 # 启动日志配置
-log_config.setup(log_file_path=str(LOG_FILE_PATH), logging_enabled=LOGGING_ENABLED)
+log_config.setup(log_file_path=str(LOG_FILE_PATH), logging_enabled=LOGGING_ENABLED,save_logs=SAVE_LOGS)
